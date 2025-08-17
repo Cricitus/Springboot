327b7b31ed3f2af0b4f1adb585f02e5783ea1958
Cricitus <158445947+Cricitus@users.noreply.github.com>
2025-08-17 21:21:31 +0800

    Add files via upload

---
+++ springboot-web-quickstart/src/main/java/com/werner/springbootwebquickstart/HelloController.java
@@ -0,0 +1,14 @@
+package com.werner.springbootwebquickstart;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //标识当前类是一个请求处理类
+public class HelloController {
+
+    @RequestMapping("/hello") //标识请求路径
+    public String hello(String name){
+        System.out.println("HelloController ... hello: " + name);
+        return "Hello " + name;
+    }
+}
